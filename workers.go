5fc7e756fa2d543fff33b5c0d67279a85892e22b
cbodonnell <cbodonnell@comcast.net>
2021-08-11 23:10:23 -0400

    clean up

--- workers.go
+++ workers.go
@@ -8,14 +8,12 @@ func handleLogs() {
 	for {
 		msg := <-logChan
 		log.Printf("LOG: %s", msg)
-		// fmt.Printf("log: %s", msg)
 	}
 }
 
 func handleFederation() {
 	for {
 		fed := <-fedChan
-		// fmt.Println("federating...")
 		fed.Federate()
 	}
 }
